d4708f38e1e551101baf5f9ed171f86ff717575b
Jessica Schultz <jessicaschultz1203@gmail.com>
2018-01-19 09:42:43 -0800

    seeded data with faker. Userstory completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.unique.password
+  )
+end
+puts "#{User.count} users created"
+
+20.times do
+  Wiki.create!(
+    title: Faker::SiliconValley.invention,
+    body: Faker::SiliconValley.quote,
+    user_id: Faker::Number.between(1,10)
+  )
+end
+wikis = Wiki.all
+puts "#{Wiki.count} wikis created"
