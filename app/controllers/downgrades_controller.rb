059f629881ccd7ae4348fa875cfff242ba3d1849
Jessica Schultz <jessicaschultz1203@gmail.com>
2018-02-07 18:06:24 -0800

    added flash div to edit profile view.

--- app/controllers/downgrades_controller.rb
+++ app/controllers/downgrades_controller.rb
@@ -3,7 +3,7 @@ class DowngradesController < ApplicationController
   end
 
   def downgrade_account
-    flash[:alert] = "Reminder: all private wikis will become public with standard plan."
+    flash[:warning] = "Reminder: all private wikis will become public with standard plan."
     current_user.update_attribute(:role, 0)
 
     @wikis = Wiki.all
