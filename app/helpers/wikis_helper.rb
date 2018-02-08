6f7f2e05aec72f7b0243a6cd836d71d41a231c2c
Jessica Schultz <jessicaschultz1203@gmail.com>
2018-02-08 13:15:36 -0800

    markdown completed

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,6 @@
 module WikisHelper
+  def markdown(text)
+    options = [:tables, :fenced_code_blocks, :autolink, :strikethrough, :underline, :highlight, :quote, :footnotes]
+    Markdown.new(text, *options).to_html.html_safe
+  end
 end
